a7b24d8cd495a55f83bc473d0f4b895b301ec941
tgalvin001 <tgalvin001@gmail.com>
2019-06-28 19:06:08 -0500

    Open 100 Doors

--- Program.cs
+++ Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 
+
+
 namespace Doors
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool[] doors = new bool[101];
+
+            for (int pass = 1; pass <= 101; pass++)
+            {
+                for (int i = 0; i < doors.Length; i++)
+                {
+                    if (i % pass == 0)
+                    {
+                        if (doors[i] == false)
+                            doors[i] = true;
+                        else
+                            doors[i] = false;
+                    }
+
+                }
+            }
+
+            for (int i = 0; i < doors.Length; i++)
+            {
+                Console.WriteLine($"{i}: {doors[i]}");
+            }
+            Console.ReadKey();
         }
     }
 }
